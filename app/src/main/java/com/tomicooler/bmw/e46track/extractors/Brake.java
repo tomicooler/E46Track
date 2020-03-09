e183da5afabef00cd5f9ec0624334bda221f4457
Tamas Domok <domoktamas@gmail.com>
2020-03-09 19:04:14 +0100

    extractors/brake: fix extraction logic.

--- app/src/main/java/com/tomicooler/bmw/e46track/extractors/Brake.java
+++ app/src/main/java/com/tomicooler/bmw/e46track/extractors/Brake.java
@@ -5,20 +5,32 @@ import com.tomicooler.bmw.e46track.ds2.Message;
 
 public class Brake implements MessageHandler {
 
-    private final Extractor extractor;
+    private final Extractor extractorIntegral;
+    private final Extractor extractorFractional;
     private final TrackModel model;
 
     public Brake(TrackModel model) {
-        extractor = new Extractor(6, false, 1, false, new Converter(3.86947, -43.2321));
+        extractorIntegral = new Extractor(6, false, 1, false, new Converter(2.56, 0));
+        extractorFractional = new Extractor(7, false, 1, false, new Converter(1.0/100.0, 0));
         this.model = model;
     }
 
     @Override
     public void process(Message message) {
-        double value = extractor.extract(message);
-        if (value > 200) {
-            value = 0.0;
+        // request for offsets b829f102210241
+        // byte pos 3 -> brake circuit 1 offset
+        // byte pos 5 -> brake circuit 2 offset
+
+        double integral = extractorIntegral.extract(message);
+
+        if (integral > 200) {
+            integral = 0.0;
         }
-        model.getCurrentBrake().postValue(value);
+        double fractional = 0.0;
+        if (integral > 0.0 ) {
+            fractional = extractorFractional.extract(message);
+        }
+
+        model.getCurrentBrake().postValue(integral + fractional);
     }
 }
