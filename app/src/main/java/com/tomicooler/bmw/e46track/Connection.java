30fd9f20c8833ddfa80b02a8c853dbfe2fc078be
Tamas Domok <domoktamas@gmail.com>
2020-03-09 06:44:38 +0100

    Connection.java: auto close socket and reconnect on error.

--- app/src/main/java/com/tomicooler/bmw/e46track/Connection.java
+++ app/src/main/java/com/tomicooler/bmw/e46track/Connection.java
@@ -14,6 +14,7 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.List;
+import java.util.Locale;
 
 public class Connection implements Runnable {
     private static final String TAG = Connection.class.getSimpleName();
@@ -33,54 +34,55 @@ public class Connection implements Runnable {
     @Override
     public void run() {
         try {
-            System.out.println(String.format("connecting '%s:%d'", address, port));
-            Socket socket = new Socket(address, port);
-            Thread.sleep(500);
-            System.out.println(String.format("connected '%s:%d'", address, port));
-            OutputStream out = socket.getOutputStream();
-            InputStream in = socket.getInputStream();
+            while (true) {
+                System.out.println(String.format(Locale.getDefault(), "Trying to connect %s:%d", address, port));
 
-            final Parser parser = new Parser();
-            byte[] buffer = new byte[255];
-            while (socket.isConnected()) {
+                try (Socket socket = new Socket(address, port)) {
+                    OutputStream out = socket.getOutputStream();
+                    InputStream in = socket.getInputStream();
 
-                for (Requester requester : requesters) {
-                    out.write(requester.getRequestMessageFramed());
-                    out.flush();
-                    System.out.println(String.format("sending '%s'", Utils.bytesToHex(requester.getRequestMessageFramed())));
+                    final Parser parser = new Parser();
+                    byte[] buffer = new byte[255];
+                    while (socket.isConnected()) {
 
-                    int size = in.read(buffer);
-                    if (size == -1) {
-                        System.out.println("size -1");
-                        Thread.sleep(200);
-                        continue;
+                        for (Requester requester : requesters) {
+                            out.write(requester.getRequestMessageFramed());
+                            out.flush();
+                            System.out.println(String.format("sending '%s'", Utils.bytesToHex(requester.getRequestMessageFramed())));
+
+                            int size = in.read(buffer);
+                            if (size == -1) {
+                                System.out.println("size -1");
+                                break;
+                            }
+
+                            try {
+                                System.out.println(String.format("try parsing '%s'", Utils.bytesToHex(buffer)));
+                                Message message = parser.parse(buffer);
+                                requester.process(message);
+                                System.out.println("parsed");
+                            } catch (NotEnoughData notEnoughData) {
+                                Log.e(TAG, "Not enough data");
+                            }
+
+                            Thread.sleep(50);
+                        }
                     }
-
-                    try {
-                        System.out.println(String.format("try parsing '%s'", Utils.bytesToHex(buffer)));
-                        Message message = parser.parse(buffer);
-                        requester.process(message);
-                        System.out.println("parsed");
-                    } catch (NotEnoughData notEnoughData) {
-                        Log.e(TAG, "Not enough data");
-                    }
-
-                    Thread.sleep(50);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Socket error", e);
+                    //model.getCurrentError().postValue(e.getMessage());
+                } catch (InvalidChecksum invalidChecksum) {
+                    invalidChecksum.printStackTrace();
+                    Log.e(TAG, "Invalid checksum");
+                    //model.getCurrentError().postValue("Invalid checksum");
                 }
+
+                System.out.println("Trying to reconnect in 500 ms");
+                Thread.sleep(500);
             }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Socket error", e);
-            model.getCurrentError().postValue(e.getMessage());
-        } catch (InvalidChecksum invalidChecksum) {
-            invalidChecksum.printStackTrace();
-            Log.e(TAG, "Invalid checksum");
-            model.getCurrentError().postValue("Invalid checksum");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 }
