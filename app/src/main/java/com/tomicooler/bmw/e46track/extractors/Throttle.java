4e86c6a1c87c77507b80e71c83d03fddebd07ffa
Tamas Domok <domoktamas@gmail.com>
2020-03-09 21:16:50 +0100

    extractors: added throttle

--- app/src/main/java/com/tomicooler/bmw/e46track/extractors/Throttle.java
+++ app/src/main/java/com/tomicooler/bmw/e46track/extractors/Throttle.java
@@ -5,16 +5,20 @@ import com.tomicooler.bmw.e46track.ds2.Message;
 
 public class Throttle implements MessageHandler {
 
-    private final Extractor extractor;
+    private final Extractor extractorIntegral;
+    private final Extractor extractorFractional;
     private final TrackModel model;
 
     public Throttle(TrackModel model) {
-        extractor = new Extractor(7, 1, false, new Converter(0.466406, 0.513281));
+        extractorIntegral = new Extractor(28, false, 1, false, new Converter(2.56, 0));
+        extractorFractional = new Extractor(29, false, 1, false, new Converter(1.0/100.0, 0));
         this.model = model;
     }
 
     @Override
     public void process(Message message) {
-        model.getCurrentThrottle().postValue(extractor.extract(message));
+        final double integral = extractorIntegral.extract(message);
+        final double fractional = extractorFractional.extract(message);
+        model.getCurrentThrottle().postValue((integral + fractional) * 10.0);
     }
 }
