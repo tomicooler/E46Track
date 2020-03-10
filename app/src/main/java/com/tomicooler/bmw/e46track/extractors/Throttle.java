d002701458417af6398573886a8a11109ab7b374
Tamas Domok <domoktamas@gmail.com>
2020-03-10 20:40:13 +0100

    Throttle.java: DKP -> PWG Voltage Poti 1.
    DKP opens slowly while the engine is running. It opens on full throttle.
    I'm not sure yet what DKP is, I have a guess, need to ask a car mechanic :)

--- app/src/main/java/com/tomicooler/bmw/e46track/extractors/Throttle.java
+++ app/src/main/java/com/tomicooler/bmw/e46track/extractors/Throttle.java
@@ -7,11 +7,13 @@ public class Throttle implements MessageHandler {
 
     private final Extractor extractorIntegral;
     private final Extractor extractorFractional;
+    private final Converter converter;
     private final TrackModel model;
 
     public Throttle(TrackModel model) {
-        extractorIntegral = new Extractor(28, false, 1, false, new Converter(2.56, 0));
-        extractorFractional = new Extractor(29, false, 1, false, new Converter(1.0/100.0, 0));
+        extractorIntegral = new Extractor(24, false, 1, false, new Converter(2.56, 0));
+        extractorFractional = new Extractor(25, false, 1, false, new Converter(1.0/100.0, 0));
+        converter = new Converter(0.295, 0.75);
         this.model = model;
     }
 
@@ -19,6 +21,6 @@ public class Throttle implements MessageHandler {
     public void process(Message message) {
         final double integral = extractorIntegral.extract(message);
         final double fractional = extractorFractional.extract(message);
-        model.getCurrentThrottle().postValue((integral + fractional) * 10.0);
+        model.getCurrentThrottle().postValue(converter.convert(integral + fractional));
     }
 }
