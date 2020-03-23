6325cb84b4cb3605d06c50e8d412c8d7d6ff8682
Tamas Domok <domoktamas@gmail.com>
2020-03-23 09:41:09 +0100

    project: removed java based android app.

--- app/src/main/java/com/tomicooler/bmw/e46track/ds2/exceptions/NotEnoughData.java
+++
@@ -1,4 +0,0 @@
-package com.tomicooler.bmw.e46track.ds2.exceptions;
-
-public class NotEnoughData extends Exception {
-}
--- app/src/main/java/com/tomicooler/bmw/e46track/extractors/MessageHandler.java
+++
@@ -1,7 +0,0 @@
-package com.tomicooler.bmw.e46track.extractors;
-
-import com.tomicooler.bmw.e46track.ds2.Message;
-
-public interface MessageHandler {
-    void process(final Message message);
-}
