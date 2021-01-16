4ab6c6d1007743f40d92712093872f691fb967bc
Tamas Domok <domoktamas@gmail.com>
2021-01-16 16:45:36 +0100

    main.cpp: explicitly ask for COARSE_LOCATION too.
    E46Track should not use BACKGROUND_LOCATION, hope this
    or the targetSdkVersion fixes it.

--- App/main.cpp
+++ App/main.cpp
@@ -17,7 +17,8 @@
 
 bool requestAndroidPermissions() {
   const QVector<QString> permissions(
-      {"android.permission.ACCESS_FINE_LOCATION",
+      {"android.permission.ACCESS_COARSE_LOCATION",
+       "android.permission.ACCESS_FINE_LOCATION",
        "android.permission.WRITE_EXTERNAL_STORAGE",
        "android.permission.READ_EXTERNAL_STORAGE"});
 
