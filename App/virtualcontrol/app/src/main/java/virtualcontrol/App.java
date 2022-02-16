55e8643ae401c10b64b5e474bf984532cee8d318
Tamas Domok <domoktamas@gmail.com>
2022-02-16 18:34:24 +0100

    App: added virtualcontrol java robot for simulating mouse actions.
    Do not forget bootstrap the project with:
    
    ./gradlew install

---
+++ App/virtualcontrol/app/src/main/java/virtualcontrol/App.java
@@ -0,0 +1,97 @@
+package virtualcontrol;
+
+import java.awt.event.InputEvent;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.LinkedList;
+import java.awt.Robot;
+
+public class App {
+
+    private static App app;
+    private static boolean running = true;
+    private Robot robot;
+    private LinkedList<Integer> pressedKeys;
+
+    public static void main(String[] args) {
+        System.out.println("##############################");
+        System.out.println("#       VirtualControl       #");
+        System.out.println("##############################");
+
+        app = new App();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutdown hook started...");
+
+            running = false;
+
+            if (app != null) {
+                app.releasePressedKeys();
+            }
+
+            System.out.println("Shutdown hook completed...");
+        }));
+
+        app.run();
+    }
+
+    public void mousePress(int keyCode) {
+        if (!pressedKeys.contains(keyCode)) {
+            robot.mousePress(keyCode);
+            pressedKeys.add(keyCode);
+        }
+    }
+
+    public void mouseRelease(int keyCode) {
+        if (pressedKeys.contains(keyCode)) {
+            robot.mouseRelease(keyCode);
+            pressedKeys.removeFirstOccurrence(keyCode);
+        }
+    }
+
+    public void releasePressedKeys() {
+        if (pressedKeys == null) {
+            return;
+        }
+
+        for (Integer pressedKey : pressedKeys) {
+            mouseRelease(pressedKey);
+        }
+    }
+
+    private void run() {
+        System.out.println("Virtual control is running!");
+
+
+        try {
+            robot = new Robot();
+            pressedKeys = new LinkedList<>();
+
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+            while (running) {
+                String str = stdin.readLine();
+                if (str == null) {
+                    break;
+                }
+                switch (str) {
+                    case "p_up": mousePress(InputEvent.BUTTON1_DOWN_MASK); break;
+                    case "r_up": mouseRelease(InputEvent.BUTTON1_DOWN_MASK); break;
+                    case "p_down": mousePress(InputEvent.BUTTON3_DOWN_MASK); break;
+                    case "r_down": mouseRelease(InputEvent.BUTTON3_DOWN_MASK); break;
+                }
+                if (str.startsWith("m_move")) {
+                    String[] split = str.split(" ");
+                    if (split.length == 3) {
+                        int x = Integer.parseInt(split[1]);
+                        int y = Integer.parseInt(split[2]);
+                        robot.mouseMove(x, y);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Virtual control is stopped!");
+    }
+}
